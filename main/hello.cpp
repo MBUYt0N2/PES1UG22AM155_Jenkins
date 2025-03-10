40758f50939c0565444fd9a1eebc2f9fd80d12e0
mbuyt0n <shusrith03@gmail.com>
2025-03-10 20:33:56 +0530

    8

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Run please\n";
+  cout << "Now work\n";
   return 0;
 }
